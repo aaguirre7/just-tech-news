1c74160b1041d4e420131a5bd7f22ba6d6bb2d0d
Abraham Aguirre <aguirre.abraham@gmail.com>
2022-03-27 23:13:10 -0400

    still does not work

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost' || '172.24.16.1' ,
+  host: 'localhost' || '172.24.16.1' || 'j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' ,
   dialect: 'mysql',
   port: 3306
 });
